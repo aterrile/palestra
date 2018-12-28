b5f8c7770c3d8cfdfea607f43595522475b75aa8
Unknown <aterrile@gmail.com>
2018-12-28 07:57:23 -0300

    Uload all

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+	$(".btnComprar").click(function(e){
+		e.preventDefault();
+		var plan = $(this).closest('section').find('h2').text();
+		var tipo = $(this).parent('.box').find('h3').text();
+		var color = $($(this).closest('section')).css('background-color');
+		var precio = $(this).parent('.box').find('span').html();
+		
+		$("#popup .boton").html('Pagar ' + plan + ' - ' + tipo + ': ' + precio).css('background-color',color);
+
+		
+		$("#popup, .overlay").fadeIn(300);
+	})
+
+	$("#popup .cerrar").click(function(e){
+		e.preventDefault();
+		$("#popup, .overlay").fadeOut(300);
+	})
+
+})
